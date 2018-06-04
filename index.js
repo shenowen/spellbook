b83381eb9bb97aa43c05019ee7cb291fc5d0969e
Owen Shen <shenowen36@gmail.com>
2018-06-04 17:24:18 -0400

    Finish basic homework

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('testing')
+
+const heading = document.querySelector('h1')
+const button = document.querySelector('button')
+
+function changeText(){
+    heading.textContent = 'changed text bois'
+    
+}
+
+button.addEventListener("click", changeText)
