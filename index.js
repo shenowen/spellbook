5a3088a66a7264ca50a43957f5432be080c75a6b
Owen Shen <shenowen36@gmail.com>
2018-06-05 00:34:36 -0400

    Finished rest of extra credit with onsubmit

--- index.js
+++ index.js
@@ -3,9 +3,16 @@ console.log('testing')
 const heading = document.querySelector('#header2')
 const button = document.querySelector('button')
 
-function changeText(){
+function changeTextBonus(){
     heading.textContent = 'changed text bois'
-    
 }
 
-button.addEventListener("click", changeText)
+button.addEventListener("click", changeTextBonus)
+
+let changingheader = document.querySelector('h2')
+//const changebutton = document.querySelector('#button2')
+
+
+function changeHeaderText(){
+    changingheader.textContent = document.getElementById('headertext').value
+}
