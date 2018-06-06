8728be8de1fb8794fa580df4634d97a6c847aeb4
Owen Shen <shenowen36@gmail.com>
2018-06-06 09:49:20 -0400

    Separate creation of 'li' and 'span's into method calls

--- index.js
+++ index.js
@@ -10,16 +10,27 @@ const addListElement = function(ev) {
   const spellsDiv = document.querySelector('#spells')
   /*spellsDiv.innerHTML += `<li>${spellName}</li>`
   spellsDiv.innerHTML += `<li>${manaCost} mana</li>` */// old way before createElement()
-  //Spellname createElement below
-  let spellNameEntry = document.createElement('li')
-  spellNameEntry.appendChild(document.createTextNode(spellName))
-  spellsDiv.appendChild(spellNameEntry)
-  //Manacost createElement below
-  let manaCostEntry = document.createElement('li')
-  manaCostEntry.appendChild(document.createTextNode(manaCost + ' Mana'))
-  spellsDiv.appendChild(manaCostEntry)
+  //Spellname createElement method call below
+  let spellEntry = createListElement()
+  //addSpan method calls for the spell and mana below
+  addSpan(spellEntry, spellName)
+  addSpan(spellEntry, ' : ' + manaCost + ' mana\n ')
+  //append into the list of spells
+  spellsDiv.appendChild(spellEntry)
 
   f.reset()
 }
+
+function createListElement(){//simply generates the li element
+  return document.createElement('li')
+}
+
+function addSpan(parentElement, string){// passes in the li element and string to create spans
+    let entrySpan = document.createElement('span')
+    entrySpan.appendChild(document.createTextNode(string + ' '))
+    parentElement.appendChild(entrySpan)
+    return entrySpan
+}
+
 
 form.addEventListener('submit', addListElement)
